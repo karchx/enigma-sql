1258d0191110c12e43680f95634368bcf4776cd2
karchx <kenethra.74@gmail.com>
2023-09-07 11:47:25 -0600

    chore: remove struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 use mysql::*;
 use mysql::prelude::*;
 
-#[derive(Debug, PartialEq, Eq)]
-struct Payment {
-    custormer_id: i32,
-    amount: i32,
-    account_name: Option<String>,
-}
-
 fn main() {
     let url = "mysql://root:root@192.168.101.8:3306/seguros";
     let pool = Pool::new(url).unwrap();
