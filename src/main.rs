015d16d86a90021d0e98875629cfc6bd3a79f961
karchx <kenethra.74@gmail.com>
2023-09-07 11:46:37 -0600

    feat: select query

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use mysql::*;
+use mysql::prelude::*;
+
+#[derive(Debug, PartialEq, Eq)]
+struct Payment {
+    custormer_id: i32,
+    amount: i32,
+    account_name: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let url = "mysql://root:root@192.168.101.8:3306/seguros";
+    let pool = Pool::new(url).unwrap();
+
+    let mut conn = pool.get_conn().unwrap();
+
+    conn
+        .query_iter("SELECT idcotizacion FROM seguros.cotizacion LIMIT 10")
+        .unwrap()
+        .for_each(|row| {
+            let r: i32 = from_row(row.unwrap());
+            println!("{}", r);
+        });
 }
