bc7dff1542f632c0ab0540ef4ad6617e80a49168
karchx <kenethra.74@gmail.com>
2023-09-08 15:10:49 -0600

    feat: mysql connect with envs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
+use dotenv::dotenv;
 use mysql::prelude::*;
 use mysql::*;
 use std::env;
 
 fn main() {
-    let url = "mysql://root:root@192.168.101.8:3306/seguros";
-    let pool = Pool::new(url).unwrap();
+    dotenv().ok();
+    let mysql_port: u16 = read_vars("MYSQL_PORT")
+        .parse()
+        .expect("please give me correct string number");
+    let mysql_user = read_vars("MYSQL_USER");
+    let mysql_pass = read_vars("MYSQL_PASS");
+    let mysql_host = read_vars("MYSQL_HOST");
+    let mysql_db = read_vars("MYSQL_DB");
+
+    let opts = OptsBuilder::new()
+        .user(Some(&mysql_user))
+        .pass(Some(&mysql_pass))
+        .ip_or_hostname(Some(&mysql_host))
+        .tcp_port(mysql_port)
+        .db_name(Some(&mysql_db));
+
+    let pool = Pool::new(opts).unwrap();
     let args: Vec<String> = env::args().collect();
     let where_param = &args[1];
 
-    println!("Searching: {}", where_param);
+    // println!("Searching: {}", where_param);
 
     let mut conn = pool.get_conn().unwrap();
 
     let select_formulas = conn
-        .query_iter("SELECT formula FROM seguros.plantilla_ramo_cobertura_tarifa WHERE idramo = 3")
-        .get();
+        .query_iter("SELECT formula FROM seguros.plantilla_ramo_cobertura_tarifa WHERE idramo = 3");
 
     //let filter = select_formulas.filter(|f| f.contains(where_param));
     println!("{:?}", select_formulas);
@@ -23,4 +38,10 @@ fn main() {
     //        let r: String = from_row(row.unwrap());
     //        println!("{}", r);
     //    });
+}
+
+fn read_vars(var: &str) -> String {
+    let error_expect = format!("{} must be set.", var);
+
+    std::env::var(var).expect(&error_expect)
 }
